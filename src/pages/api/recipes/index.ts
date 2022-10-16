16f4c02ebdd995354b2e08d571925fc4b491f1ed
Franklin Jara <franklinjaradev@gmail.com>
2022-10-15 23:18:23 -0400

    :zap: feat: adding authentication to api

--- src/pages/api/recipes/index.ts
+++ src/pages/api/recipes/index.ts
@@ -5,21 +5,32 @@ import { env } from "src/env/server.mjs";
 import { RECIPE_LIST_URL } from "src/constants";
 
 const recipes = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "GET") {
-    const { search } = req.query;
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RAPIDAPI-KEY": env.RAPID_API_KEY,
-        "X-RAPIDAPI-HOST": env.RAPID_API_HOST,
-      },
-    };
+  const session = await getServerAuthSession({ req, res });
 
-    const response = await fetch(RECIPE_LIST_URL + search, options);
+  if (session) {
+    if (req.method === "GET") {
+      const { search } = req.query;
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RAPIDAPI-KEY": env.RAPID_API_KEY,
+          "X-RAPIDAPI-HOST": env.RAPID_API_HOST,
+        },
+      };
 
-    const data = await response.json();
+      const response = await fetch(RECIPE_LIST_URL + search, options);
 
-    res.status(200).json(data);
+      const data = await response.json();
+
+      res.status(200).json(data);
+    }
+  } else {
+    res
+      .status(401)
+      .json({
+        error:
+          "You must be signed in to view the protected content on this page.",
+      });
   }
 };
 
