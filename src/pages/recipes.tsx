a6b7a95442b7ba381fa6e3474a26d15a2a460337
Franklin Jara <franklinjaradev@gmail.com>
2022-11-30 17:44:13 -0500

    :sparkles: feat: adding search input for recipes

--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -1,4 +1,5 @@
 import { GetServerSidePropsContext } from "next";
+import { useCallback, useEffect, useState } from "react";
 import Head from "next/head";
 
 import { trpc } from "src/utils/trpc";
@@ -9,6 +10,23 @@ import Error from "src/components/error";
 
 const Recipes = () => {
   const { data, isLoading, isError } = trpc.recipe.getAllRecipes.useQuery();
+  const [search, setSearch] = useState("");
+  const [recipesData, setRecipesData] = useState(data);
+
+  const findRecipes = () => {
+    return (
+      data &&
+      data.filter((recipe) => {
+        return recipe.name.toLowerCase().includes(search.toLowerCase());
+      })
+    );
+  };
+
+  const findRecipesCallback = useCallback(findRecipes, [search, data]);
+
+  useEffect(() => {
+    setRecipesData(findRecipesCallback());
+  }, [findRecipesCallback]);
 
   return (
     <div className="flex flex-col gap-4">
@@ -16,9 +34,18 @@ const Recipes = () => {
         <title>Recipes</title>
       </Head>
       <h1 className="text-center text-3xl font-bold">Recipes</h1>
+      <div className="flex justify-center gap-2 pb-4">
+        <input
+          type="text"
+          className="custom-border rounded bg-white bg-opacity-40 p-2"
+          placeholder="Type to find a recipe"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+      </div>
       {isLoading && <Spinner text="Loading Recipes" />}
       {isError && <Error />}
-      {data && <Recipe data={data} />}
+      {recipesData && <Recipe data={recipesData} />}
     </div>
   );
 };
