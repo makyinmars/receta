6341ad2772eb8cc8a256f4dcc0e491e00d2c256f
Franklin Jara <franklinjaradev@gmail.com>
2022-12-20 18:45:48 -0500

    :sparkles: feat: style for react player mobile support

--- src/components/dynamic-player.tsx
+++ src/components/dynamic-player.tsx
@@ -4,13 +4,15 @@ const DynamicPlayer = () => {
   return (
     <div className="flex flex-col items-center justify-around gap-4 md:flex-row">
       <ReactPlayer
+        width={350}
         controls={true}
-        className="h-80 w-[500px] self-center rounded"
+        className="self-center rounded"
         url="https://www.youtube.com/watch?v=AcpqpzsELUE"
       />
       <ReactPlayer
+        width={350}
         controls={true}
-        className="h-80 w-[500px] self-center rounded"
+        className="h-80 self-center rounded"
         url="https://www.youtube.com/watch?v=7946ogTfAM4"
       />
     </div>
